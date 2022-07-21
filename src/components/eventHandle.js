af2f5abe684646ac7645be9c594fe27b0709b787
lokesh778 <lokesh.thadikamalla@codexbox.com>
2022-07-21 13:28:25 +0530

    event Handel completed

--- src/components/eventHandle.js
+++ src/components/eventHandle.js
@@ -1,30 +1,96 @@
 import React, { useState } from 'react'
+import img from '../image/download .jpg'
 
 function EventHandel() {
-    let [count,setCount]=useState({
-        counter:0
+    const [product, setProduct] = useState({
+        Sno: "1Azxx",
+        image: '',
+        name: 'Watch',
+        price: 1200,
+        qty: 0,
+        total: 1200
+
     })
-    const increment=()=>{
+    let [count, setCount] = useState(0)
+    let [message, setMessage] = useState('Hi')
+    // let counter = 0
+    let {total}=product
+    const increment = () => {
+        total=total+1200
+        setProduct({...product,total})
 
-        setCount({...count,state.counter})
-
+        count++
+        setCount(count)
     }
-    const decrement=()=>{
-        setCount(count--)
+    const decrement = () => {
+        count--
+        setProduct({...product,})
+        setCount(count)
+    }
+    const gm = (value) => {
+        setMessage(value)
     }
 
-  return (
-    <div>
-        <h1>
-            Counter 
-        </h1>
-        <button  onClick={increment}>
-            Increment 
-        </button>
-        <h1>{count}</h1>
-        <button onClick={decrement}>Decrement</button>
-    </div>
-  )
+   
+
+
+
+
+    return (
+        <div>
+            <h1>
+                Counter
+            </h1>
+            <button onClick={increment}>
+                Increment
+            </button>
+            <h1>{count}</h1>
+            <button onClick={decrement}>Decrement</button>
+
+            <div >
+                <h1>Wish </h1>
+
+                <h1>{message}</h1>
+                <button onClick={() => gm("Good Morning")}>Good Morning</button>
+                <button onClick={() => gm("Good Afternoon")}>Good Afternoon</button>
+
+                <button onClick={() => gm("Good Evening")}>Good Evening</button>
+
+            </div>
+            <div>
+                <h1>Prodect Iteams</h1>
+
+            </div>
+            <div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Sno</th>
+                            <th>Image</th>
+                            <th>Name</th>
+                            <th>Price</th>
+                            <th>Qty</th>
+                            <th>Total Amount</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>{product.Sno}</td>
+                            <td><img src={product.img} alt='watch' /></td>
+                            <td>{product.name}</td>
+                            <td>{product.price}</td>
+                            <td><button onClick={decrement}>-</button>{count}<button  onClick={increment}>+</button></td>
+                            <td>{product.total}</td>
+
+
+                            
+                        </tr>
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    )
 }
 
 export default EventHandel
