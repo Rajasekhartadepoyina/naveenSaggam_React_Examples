b33866bfc33e925518feff266a93ca0978ebae92
Saicharanchintala <saicharan.chintala@codexbox.com>
2022-07-21 18:11:55 +0530

    changes

--- src/components/serverConnection/userServer2.js
+++ src/components/serverConnection/userServer2.js
@@ -1,8 +1,8 @@
-import {Axios} from 'axios';
+import axios from 'axios';
 class UserServices{
     static getAllUsers(){
         let serverUrl='https://jsonplaceholder.typicode.com/users';
-        return Axios.get(serverUrl);
+        return axios.get(serverUrl);
     }
 }
 export default UserServices;
