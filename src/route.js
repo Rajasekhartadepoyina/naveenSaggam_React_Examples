b471ded3dee5e06fa61ea624e60dda5932f356ab
KlakshmiReddy <lakshmireddykeesari@gmail.com>
2022-07-21 15:07:40 +0530

    TableMapping using axios api call

--- src/route.js
+++ src/route.js
@@ -11,6 +11,7 @@ import Digitalwatch from "./components/Digitalwatch";
 import EventHandel from "./components/eventHandle";
 import Student from "./components/student";
 import Parent from "./components/parent";
+import TableMapping from "./components/mapTheTable/TableMapping";
 
 
 const AppRoutes = () => {
@@ -23,6 +24,7 @@ const AppRoutes = () => {
         <Route path="/parent_to_child" element={<Parent />} />
         <Route path="/ParentToChild" element={<ParentToChild />} />
         <Route path="/digitalwatch" element={<Digitalwatch />} />
+        <Route path="/tablemapping" element={<TableMapping />} />
       </Routes>
     </Router>
   );
