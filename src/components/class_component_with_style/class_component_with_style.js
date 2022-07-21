656e9df028e13d071800a5c7b0ee9fe4e871a853
venky106 <venkateshwarlu.kethi@codexbox.com>
2022-07-21 15:12:06 +0530

    9 and 12 completed

---
+++ src/components/class_component_with_style/class_component_with_style.js
@@ -0,0 +1,26 @@
+import React from "react"
+import "../class_component_with_style/naveen_saggam.css"
+
+class Wishcard extends React.Component {
+    render() {
+        return (
+            <>
+            <div className="navbar">
+            <span>wlecome to class based component</span>
+            </div>
+            <div className="card">
+                <div className="card-body">
+                    <h2>Welcome to class based compnent</h2>
+                    <p>Class-based components are the bread and butter of most modern web apps built in ReactJS.
+                        These components are simpl classes (made up of multiple functions that add functionality to the application).
+                        All class-based components are child classes for the Component class of ReactJS.
+                        Example: Program to demonstrate the creation of class-based components
+                    </p>
+                </div>
+                </div>
+
+            </>
+        )
+    }
+}
+export default Wishcard
--- src/route.js
+++ src/route.js
@@ -6,6 +6,8 @@ import {
   Navigate,
 } from "react-router-dom";
 import EventHandel from "./components/eventHandle";
+import ClassComonent from "./components/class_component_with_style/class_component_with_style"
+import StateManagementClass from "./components/class_state_management/state_management_class_based"
 
 
 const AppRoutes = () => {
@@ -13,6 +15,10 @@ const AppRoutes = () => {
     <Router>
       <Routes>
         <Route path="/event" element={<EventHandel />} />
+        
+        
+        <Route path="/classcomponent" element={<ClassComonent></ClassComonent>}/>
+        <Route path="/statemanagementclass" element={<StateManagementClass></StateManagementClass>}></Route>
       </Routes>
     </Router>
   );
