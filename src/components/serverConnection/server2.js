cc6efc930f05e2d4c8ff1f6c458a627a364f17b1
Saicharanchintala <saicharan.chintala@codexbox.com>
2022-07-21 18:26:43 +0530

    commit

--- src/components/serverConnection/server2.js
+++ src/components/serverConnection/server2.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useState, useEffect } from "react";
+import { Table } from "react-bootstrap";
 import UserServices from '../serverConnection/userServer2';
 
 let Server1 = () => {
@@ -8,9 +9,13 @@ let Server1 = () => {
     useEffect(() => {
 
         UserServices.getAllUsers().then((responce) => {
+
             console.log(responce);
-        }).catch(()=>{
-            
+            setState(() => ({
+                user: responce.data
+            }));
+        }).catch((error) => {
+            console.log(error);
         })
 
 
@@ -37,8 +42,39 @@ let Server1 = () => {
             <div className="container">
                 <div className="row">
                     <div className="col">
-                        <h2>welcome to fragment</h2>
-                        <pre>{JSON.stringify(state.user)}</pre>
+                        {/* <h2>welcome to fragment</h2>
+                        <pre>{JSON.stringify(state.user)}</pre> */}
+
+                        <Table className="table table-hover text=center teble-striped">
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>NAME</th>
+                                    <th>EMAIL</th>
+                                    <th>STREET</th>
+                                    <th>CITY</th>
+                                    <th>WEBSITE</th>
+                                    
+                                </tr>
+                            </thead>
+                            <tbody>
+                               {
+                                state.user.length>0 &&
+                                state.user.map(each=>{
+                                    return(
+                                        <tr key={each}>
+                                            <td>{each.id}</td>
+                                            <td>{each.name}</td>
+                                            <td>{each.email}</td>
+                                            <td>{each.address.street}</td>
+                                            <td>{each.address.city}</td>
+                                            <td>{each.website}</td>
+                                        </tr>
+                                    )
+                                })
+                               }
+                            </tbody>
+                        </Table>
                     </div>
                 </div>
             </div>
