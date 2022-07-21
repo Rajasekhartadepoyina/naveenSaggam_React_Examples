e3c5bccae7047146b46614db4c3d18ace6a48bfe
Shankar752 <shankar.boddu@codexbox.com>
2022-07-21 15:54:15 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,56 +1,25 @@
-// import logo from './logo.svg';
-// import './App.css';
+import logo from './logo.svg';
+import './App.css';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-import React from 'react'
-import { Router, Routes, Route, Navigate, } from 'react-router-dom'
-
-
-import Navbar from './components/navbar'
-import Home from './components/modules/layout/components/home'
-import Employeeslist from './components/modules/employees/components/employeeslist'
-import About from './components/modules/layout/components/about'
-import Userlogin from './components/modules/users/components/userlogin'
-import Userregister from './components/modules/users/components/userregister'
-
-const App = () => {
+function App() {
   return (
-    <Router>
-    <Navbar />
-      <Routes>
-        
-        <Route  path={'/'} component={Home}/>
-        <Route exact={true} path={'/employeeslist'} component={Employeeslist}/>
-        <Route exact={true} path={'/about'} component={About}/>
-        <Route exact={true} path={'/users/login'} component={Userlogin}/>
-        <Route exact={true} path={'/users/register'} component={Userregister}/>
-        <Route exact={true} path={'/'} component={Home}/>
-      </Routes>
-    </Router>
-  )
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/route.js
+++ src/route.js
@@ -10,12 +10,12 @@ import Condition from "./components/conditional_list_render/conditional_list_ren
 import Digitalwatch from "./components/Digitalwatch";
 import EventHandel from "./components/eventHandle";
 import Navbar from './components/navbar'
-import Home from './components/modules/layout/components/home'
-import Employeeslist from './components/modules/employees/components/employeeslist'
-import About from './components/modules/layout/components/about'
-import Userlogin from './components/modules/users/components/userlogin'
-import Userregister from './components/modules/users/components/userregister'
-import Pagenotfound from "./components/modules/layout/components/pagenotfound";
+import Home from './components/modules/layout/home'
+import Employeeslist from './components/modules/employees/employeeslist'
+import About from './components/modules/layout/about'
+import Userlogin from './components/modules/users/userlogin'
+import Userregister from './components/modules/users/userregister'
+import Pagenotfound from "./components/modules/layout/pagenotfound";
 import Student from "./components/student";
 import Parent from "./components/parent";
 import TableMapping from "./components/mapTheTable/TableMapping";
@@ -24,7 +24,7 @@ import TableMapping from "./components/mapTheTable/TableMapping";
 const AppRoutes = () => {
   return (
     <Router>
-      <Navbar />
+      {/* <Navbar /> */}
       <Routes>
         <Route path="/event" element={<EventHandel />} />
         <Route path="/" element={<Home />} />
