8a88b5a3c3a9970af04b3b6649d09b55bc2cfb9d
RajasekharT <rajasekhar.tadepoyina@codexbox.com>
2022-07-21 13:19:13 +0530

    My task is completed

--- src/components/conditional_list_render/conditional_list_render.js
+++ src/components/conditional_list_render/conditional_list_render.js
@@ -1,12 +1,13 @@
 import React from 'react'
+import AuthUser from './auth_user'
 
 const Condition = () => {
     return (
         <div>
             <nav className='navbar navbar-dark bg-dark navbar-expand-sm'>
-                <a href="/" className='navbar-brand'>React with Conditional & list Rendering</a>
+                <a href="#" className='navbar-brand'>React with Conditional & list Rendering</a>
             </nav>
-
+            <AuthUser />
         </div>
     )
 }
