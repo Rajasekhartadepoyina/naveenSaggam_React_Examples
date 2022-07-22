b463e619aa487fc0942c8062b1c3cc1ae7f68251
Dharanesh Midatala <dharanu.m@gmail.com>
2022-07-22 11:09:02 +0530

    Changes

--- src/components/Digitalwatch.js
+++ src/components/Digitalwatch.js
@@ -1,39 +1,35 @@
-import React, { useEffect, useState } from 'react'
-
-
+import React, { useEffect, useState } from "react";
 
 let Digitalwatch = () => {
+  let [state, setState] = useState({
+    currentTime: new Date().toLocaleTimeString(),
+  });
 
-    let [state, setState] = useState({
-        currentTime: new Date().toLocaleTimeString()
-    });
+  useEffect(() => {
+    let interval = setInterval(() => {
+      setState(() => ({
+        currentTime: new Date().toLocaleTimeString(),
+      }));
+    }, 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
 
-    useEffect(() => {
-        let interval = setInterval(() => {
-            setState(() => ({
-                currentTime: new Date().toLocaleTimeString()
-            }))
-        }, 1000);
-        return () => {
-            clearInterval(interval);
-        }
-    }, []);
-
-    return (
-        <React.Fragment>
-            <div className='container mt-3 w-25'>
-                <div className='card shadow-lg text-center'>
-                    <div className='card-header bg-warning'>
-                        <p className='h4'> Digital Watch </p>
-                    </div>
-                    <div>
-                        <h3> {state.currentTime} </h3>
-                    </div>
-                </div>
-            </div>
-
-        </React.Fragment>
-    )
-}
+  return (
+    <React.Fragment>
+      <div className="container mt-3 w-25">
+        <div className="card shadow-lg text-center">
+          <div className="card-header bg-warning">
+            <p className="h4"> Digital Watch </p>
+          </div>
+          <div>
+            <h3> {state.currentTime} </h3>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
 
 export default Digitalwatch;
